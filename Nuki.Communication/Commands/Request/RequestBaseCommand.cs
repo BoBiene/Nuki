63c58bd13022ebc03d81796f2050fdcedb609a9f
Bo Biene <biene@inray.de>
2016-11-06 13:06:57 +0100

    Naming

--- Nuki.Communication/Commands/Request/RequestBaseCommand.cs
+++ Nuki.Communication/Commands/Request/RequestBaseCommand.cs
@@ -8,7 +8,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Reflection;
 
-namespace Nuki.Communication.Commands
+namespace Nuki.Communication.Commands.Request
 {
     public abstract class RequestBaseCommand
     {
